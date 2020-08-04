c53a65831c8c7ce73e9eb2474b8d3cb281b787b3
Alexandreleclercq <66735824+Alexandreleclercq@users.noreply.github.com>
2020-08-04 12:38:27 +0200

    Add files via upload

---
+++ script-09.js
@@ -0,0 +1,9 @@
+let maFonction = function() {
+  alert('La fonction alert est déclenchée dans la fonction contenue dans la variable maFonction');
+};
+
+let myFunction = function() {
+  alert('Alerte déclenchée avant l\'affichage du deuxième paragraphe');
+};
+
+
